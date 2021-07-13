a6fb1acff5c4e33fe264bb57311c7d27365fcfbc
slava bily <slavabily@me.com>
2021-07-13 11:19:17 +0300

    log out functionality from DialogsView is implemented

---
+++ QuickBloxSwiftUIChat/Views/Dialogs/DialogsConstant.swift
@@ -0,0 +1,19 @@
+//
+//  DialogsConstant.swift
+//  QuickBloxSwiftUIChat
+//
+//  Created by slava bily on 11.07.2021.
+//
+
+import Foundation
+
+struct DialogsConstant {
+    static let dialogsPageLimit:Int = 100
+    static let segueGoToChat = "goToChat"
+    static let selectOpponents = "SelectOpponents"
+    static let infoSegue = "PresentInfoViewController"
+    static let deleteChats = "Delete Chats"
+    static let forward = "Forward to"
+    static let deleteDialogs = "deleteDialogs"
+    static let chats = "Chats"
+}
--- QuickBloxSwiftUIChat/Views/Dialogs/DialogsView.swift
+++ QuickBloxSwiftUIChat/Views/Dialogs/DialogsView.swift
@@ -9,16 +9,11 @@ import SwiftUI
 
 struct DialogsView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    
     private let chatManager = ChatManager.instance
     
     @State private var dialogs: [QBChatDialog] = []
-    
-    var cells: [DialogTableViewCellModel] {
-        let cells = dialogs.map {
-            DialogTableViewCellModel(dialog: $0)
-        }
-        return cells
-    }
     
     var body: some View {
         NavigationView {
@@ -27,10 +22,107 @@ struct DialogsView: View {
                     Text(cell.textLabelText)
                 }
              }
-            .navigationBarTitle("Dialogs View", displayMode: .inline)
+            .navigationBarItems(leading:
+                                    Button(action: {
+                                        didTapLogout()
+                                    }, label: {
+                                         Image("exit")
+                                    }),
+                                trailing: HStack {
+                                    Button {
+                                         
+                                    } label: {
+                                         
+                                    }
+                                    Button {
+                                        
+                                    } label: {
+                                        
+                                    }
+                                })
+            .navigationBarTitle(navigationTitle, displayMode: .inline)
             .onAppear {
                 dialogs = chatManager.storage.dialogsSortByUpdatedAt()
             }      
+        }
+    }
+    
+    private var navigationTitle: String {
+        let currentUser = Profile()
+        guard currentUser.isFull == true else {
+            return "Unknown user"
+        }
+        return currentUser.fullName.count > 0 ? currentUser.fullName : currentUser.login
+    }
+    
+    private var cells: [DialogTableViewCellModel] {
+        let cells = dialogs.map {
+            DialogTableViewCellModel(dialog: $0)
+        }
+        return cells
+    }
+    
+     private func didTapLogout() {
+        guard Reachability.instance.networkConnectionStatus() != .notConnection else {
+            debugPrint(LoginConstant.checkInternetMessage)
+//            showAlertView(LoginConstant.checkInternet, message: LoginConstant.checkInternetMessage)
+            return
+        }
+        
+        SVProgressHUD.show(withStatus: "SA_STR_LOGOUTING".localized)
+        SVProgressHUD.setDefaultMaskType(.clear)
+        
+        guard let identifierForVendor = UIDevice.current.identifierForVendor else {
+            return
+        }
+        let uuidString = identifierForVendor.uuidString
+        #if targetEnvironment(simulator)
+        disconnectUser()
+        #else
+        QBRequest.subscriptions(successBlock: { (response, subscriptions) in
+            if let subscriptions = subscriptions {
+                for subscription in subscriptions {
+                    if let subscriptionsUIUD = subscriptions.first?.deviceUDID,
+                       subscriptionsUIUD == uuidString,
+                       subscription.notificationChannel == .APNS {
+                        self.unregisterSubscription(forUniqueDeviceIdentifier: uuidString)
+                        return
+                    }
+                }
+            }
+            self.disconnectUser()
+            
+        }) { (response) in
+            if response.status.rawValue == 404 {
+                self.disconnectUser()
+            }
+        }
+        #endif
+    }
+    
+    //MARK: - logOut flow
+    private func disconnectUser() {
+        QBChat.instance.disconnect(completionBlock: { error in
+            if let error = error {
+                SVProgressHUD.showError(withStatus: error.localizedDescription)
+                return
+            }
+            self.logOut()
+        })
+    }
+    
+    private func logOut() {
+        QBRequest.logOut(successBlock: { response in
+            //ClearProfile
+            Profile.clearProfile()
+            self.chatManager.storage.clear()
+//            CacheManager.shared.clearCache()
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(1)) {
+                presentationMode.wrappedValue.dismiss()
+            }
+            SVProgressHUD.showSuccess(withStatus: "SA_STR_COMPLETED".localized)
+        }) { response in
+            debugPrint("[DialogsViewController] logOut error: \(response)")
         }
     }
 }
